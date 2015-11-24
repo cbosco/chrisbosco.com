070440dc557695c798ee2ad722726b4b9c2652e4
Chris Bosco <cbosco@ondeck.com>
2015-11-24 00:17:15 -0500

    Deploy script fixes for cleanup

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@
 # ~/Projects/cbosco.github.com local folder
 
 # Requires broccoli-cli. npm install -g broccoli-cli
-echo DEPLOY_REPO_DIR = '~/Projects/cbosco.github.com'
-echo BUILD_TEMP_DIR = ./dist
-echo BUILD_REPO_DIR = ${PWD}
+DEPLOY_REPO_DIR="/Users/chris/Projects/cbosco.github.com"
+BUILD_TEMP_DIR=./dist
+BUILD_REPO_DIR=${PWD}
 
-cd $DEPLOY_REPO_DIR && git rm -rf .
+cd $DEPLOY_REPO_DIR && git reset . && git checkout . && git rm -rf .
 rm -rf $BUILD_TEMP_DIR
 cd $BUILD_REPO_DIR && export BROCCOLI_ENV=production && broccoli build $BUILD_TEMP_DIR
-mv -f ${$BUILD_TEMP_DIR}/* DEPLOY_REPO_DIR
+mv -f ${BUILD_TEMP_DIR}/* $DEPLOY_REPO_DIR
 #cd $DEPLOY_REPO_DIR && git add . && git commit -m "Compiled asset update from projects-chrisbosco.github.com"
 
+rm -rf ${BUILD_TEMP_DIR} # clean up in case of failure
